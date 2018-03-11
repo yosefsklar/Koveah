985072c6497614673a963584c3889e411668e164
Yair Fax <yair@Yairs-MBP.dkn.io>
2018-03-11 11:20:50 -0400

    hopefully yairs picture taking works now

--- testpictureapp/app/src/main/java/com/example/yair/testpictureapp/MainActivity.java
+++ testpictureapp/app/src/main/java/com/example/yair/testpictureapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -16,6 +17,7 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import java.io.File;
@@ -25,19 +27,24 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button takePictureButton;
+    private Button viewPictureButton;
+    private ImageView picture;
 
-    Button takePictureButton;
+
     private static final String LOG_TAG = "TakePictureTest";
 
     private static final int REQUEST_TAKE_PHOTO = 1;
     private String mCurrentPhotoPath;
+    private LinearLayout layout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LinearLayout layout = new LinearLayout(this);
+        layout = new LinearLayout(this);
 
         String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
@@ -46,12 +53,36 @@ public class MainActivity extends AppCompatActivity {
         if (!hasPermissions(this, permissions))
             ActivityCompat.requestPermissions(this, permissions, PERMISSION_ALL);
 
+        picture = new ImageView(this);
+
+        viewPictureButton = new Button(this);
+        viewPictureButton.setText("View Picture");
+
+        viewPictureButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                picture.setImageBitmap(BitmapFactory.decodeFile(getExternalFilesDir(Environment.DIRECTORY_PICTURES) + "/takenpicture.jpg"));
+
+                layout.addView(picture, new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        0));
+
+                setContentView(layout);
+            }
+        });
 
         takePictureButton = new Button(this);
         takePictureButton.setText("Take Picture");
         takePictureButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 dispatchTakePictureIntent();
+
+                layout.addView(viewPictureButton, new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        0));
+
+                setContentView(layout);
             }
         });
 
